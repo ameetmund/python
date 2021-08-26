1e43e50ff38130bc81ca9a7ff00522a432c99c4e
ameetmund <42780136+ameetmund@users.noreply.github.com>
2021-08-26 06:13:27 +0530

    new code

---
+++ dev_20210826.py
@@ -0,0 +1,14 @@
+# Web scrapping
+
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://news.ycombinator.com/news')
+# print(res.text)
+soup = BeautifulSoup(res.text, 'html.parser')
+
+#print(soup.find(id='score_28306390'))
+
+story = soup.select('.storylink')
+score = soup.select('.score')
+print(score[0])
