3c91850b3947d054e7259c84bb1c6a91e1791613
ameetmund <42780136+ameetmund@users.noreply.github.com>
2023-08-19 07:11:58 +0530

    New code

---
+++ PythonBasic/level2/random_number.py
@@ -0,0 +1,8 @@
+import random
+
+# print(random.choice(range(10)))
+# print(random.choice([1,2,3,4,5,6]))
+# print(random.choice("Ameet"))
+
+# print(random.randrange(1, 10))
+print(random.random())
